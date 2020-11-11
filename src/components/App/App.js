89fd87d197f10ee7a1c56817af91980f4a17cd2c
cecil <ola@example.com>
2020-11-11 16:24:23 +0100

    avenous part four - Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-
-const business = {
-    imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-const businesses = [business, business, business, business, business, business];
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      businesses: []
+    };
+    this.SearchYelp = this.SearchYelp.bind(this);
+  }
 
   SearchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then((businesses) => {
+      this.setState({
+        businesses: businesses
+      })
+    });
   }
 
   render() {
@@ -28,7 +26,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
           <SearchBar SearchYelp={this.SearchYelp}/ >
-          <BusinessList businesses={ businesses } />
+          <BusinessList businesses={ this.state.businesses } />
       </div>
     );
   }
